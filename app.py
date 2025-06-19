1dd011a7126f8c7357c0912d5e02ed833bb5244f
Ken Clements <kenclements@gwu.edu>
2025-06-19 18:27:08 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request
+import logging
+
+app = Flask(__name__)
+
+# Set up basic file logging
+logging.basicConfig(
+    filename='app.log',        # Log file name
+    level=logging.INFO,        # Log level
+    format='%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+)
+
+@app.route('/')
+def home():
+    app.logger.info('Home page accessed')
+    return 'Hello, Flask with logging!'
+
+@app.route('/admin')
+def admin():
+    app.logger.info('ALERT!!! Admin page accessed')
+    return 'You should not be here!'
+
+@app.route('/log')
+def log_example():
+    user_ip = request.remote_addr
+    app.logger.info(f'Log endpoint accessed from {user_ip}')
+    return 'Check the log file!'
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0', port=50001)
